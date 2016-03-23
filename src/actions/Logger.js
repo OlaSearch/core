3aa049006ec9130fbc85034f2254bb59b3790c9a
Vinay <rmdort@gmail.com>
2016-03-23 14:53:50 +0800

    Refactoring

--- src/actions/Logger.js
+++ src/actions/Logger.js
@@ -33,35 +33,35 @@ var data = {
  */
 
 
-var debounceLogger = debounce( submitLog, 1000);
+var debounceLogger = debounce(submitLog, 1000);
 
-export function log( eventType, result, eventSource ){
+export function log(eventType, result, eventSource) {
 
-    return ( dispatch, getState) => {
+    return (dispatch, getState) => {
 
-        switch(eventType){
+        switch (eventType) {
 
             case 'C': // Click event
-                submitLog( dispatch, getState, eventType, result, eventSource)        
+                submitLog(dispatch, getState, eventType, result, eventSource)
                 break;
 
             default:
-                debounceLogger( dispatch, getState, eventType, result, eventSource);
+                debounceLogger(dispatch, getState, eventType, result, eventSource);
                 break
-        }        
-        
+        }
+
     }
 }
 
 /**
  * Log submit function 
  */
-function submitLog(){
+function submitLog() {
 
-    var [ dispatch, getState, eventType, result, eventSource ] = arguments;
+    var [dispatch, getState, eventType, result, eventSource] = arguments;
 
     dispatch({
-        log:  true,
+        log: true,
         type: 'SEND_LOG',
         eventType: eventType,
         result,
--- src/components/AutoSuggest/FacetSuggestion.js
+++ src/components/AutoSuggest/FacetSuggestion.js
@@ -2,45 +2,45 @@ import React from 'react';
 
 const FacetSuggestion = ( props ) => {
 
-    var { facets, query, name, addFacet, dispatch, onSubmit, limit } = props;
+	var { facets, query, name, addFacet, dispatch, onSubmit, limit } = props;
 
-    var facet = facets.filter( item => item.name == name);
-    var values = []
-        .concat
-        .apply([], facet.map( item => item.values))
-        .splice(0, limit)
+	var facet = facets.filter( item => item.name == name);
+	var values = []
+		.concat
+		.apply([], facet.map( item => item.values))
+		.splice(0, limit)
 
-    var { q } = query;
+	var { q } = query;
 
-    return (
-        <div className="ola-facet-suggestions">
-            {values.map( (value, idx) => {
+	return (
+		<div className="ola-facet-suggestions">
+			{values.map( (value, idx) => {
 
-                return (
-                    <a
-                        className="ola-facet-suggestion"
-                        key = { idx }
-                        tabIndex = {0}
-                        onClick = { () => {
+				return (
+					<a
+						className="ola-facet-suggestion"
+						key = { idx }
+						tabIndex = {0}
+						onClick = { () => {
 
-                            dispatch( addFacet(facet[0], value.name) )
+							dispatch( addFacet(facet[0], value.name) )
 
-                            /* Prevent race condition */
+							/* Prevent race condition */
 
-                            setTimeout( () => { onSubmit && onSubmit() }, 0)
+							setTimeout( () => { onSubmit && onSubmit() }, 0)
 
-                        }}
-                        >
-                        <strong>{ q }</strong> in {value.name}
-                    </a>
-                )
-            })}
-        </div>
-    )
+						}}
+						>
+						<strong>{ q }</strong> in {value.name}
+					</a>
+				)
+			})}
+		</div>
+	)
 };
 
 FacetSuggestion.defaultProps = {
-    limit: 3
+	limit: 3
 }
 
 module.exports = FacetSuggestion
