4bcd83e1cc52c1d893303aac135ac47129b1648e
Vinay <rmdort@gmail.com>
2016-05-01 16:32:55 +0800

    updates

--- src/components/Snippets/NoResults.js
+++ src/components/Snippets/NoResults.js
@@ -5,15 +5,21 @@ class NoResults extends React.Component {
     results: React.PropTypes.array,
     isLoading: React.PropTypes.bool,
     q: React.PropTypes.string
-  }
+  };
+
   render () {
-    var { results, isLoading, q } = this.props
+    var { results, isLoading, q, isBookmark } = this.props
 
     if (results.length || isLoading) return null
 
+    let message = `No results found matching ${q}. Please try again.`
+    if (isBookmark) {
+      message = `You do not have any bookmarks. Click on the heart icon to add one.`
+    }
+
     return (
       <div className='ola-snippet ola-snippet-noresults'>
-        No results found matching <strong>{q}</strong>
+        {message}
       </div>
     )
   }
