c389bca6ee107688003f9da2d8cc8e20b7b2e697
Vinay <rmdort@gmail.com>
2016-05-11 11:53:34 +0800

    Fixed a bug in facet toggle controller

--- src/decorators/OlaFacetToggle.js
+++ src/decorators/OlaFacetToggle.js
@@ -4,7 +4,15 @@ export const FacetToggle = (ComposedComponent) => class extends React.Component 
   constructor (props) {
     super(props)
     this.state = {
-      isCollapsed: false
+      isCollapsed: props.facet.isCollapsed || false
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.facet.isCollapsed !== this.props.facet.isCollapsed) {
+      this.setState({
+        isCollapsed: nextProps.facet.isCollapsed
+      })
     }
   }
 
