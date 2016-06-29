247eba463f008c138868ccadf1a01777816199bd
Vinay <rmdort@gmail.com>
2016-06-29 16:17:26 +0800

    Updated route

--- src/decorators/OlaRoute.js
+++ src/decorators/OlaRoute.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import { initSearch } from './../actions/Search'
+import { getComponentDisplayName } from './../utilities'
+import hoistNonReactStatics from 'hoist-non-react-statics'
 
-export const OlaRoute = (ComposedComponent) => class extends React.Component {
-  static contextTypes = {
-    config: React.PropTypes.object,
-    router: React.PropTypes.object
-  };
-  componentWillMount () {
-    if (!this.context.router) window.addEventListener('popstate', this.onPopState)
+module.exports = (WrappedComponent) => {
+  class OlaRoute extends React.Component {
+    static contextTypes = {
+      config: React.PropTypes.object,
+      router: React.PropTypes.object
+    };
+    static displayName = `olaRoute(${getComponentDisplayName(WrappedComponent)})`;
+    componentWillMount () {
+      if (!this.context.router) window.addEventListener('popstate', this.onPopState)
+    }
+    componentWillUnmount () {
+      if (!this.context.router) window.removeEventListener('popstate', this.onPopState)
+    }
+    componentWillReceiveProps (nextProps) {
+      if (this.context.router && this.props.location.search !== nextProps.location.search) this.onPopState()
+    }
+    onPopState = () => {
+      this.props.dispatch(initSearch({ config: this.context.config }))
+    };
+    render () {
+      return <WrappedComponent {...this.props} />
+    }
   }
-  componentWillUnmount () {
-    if (!this.context.router) window.removeEventListener('popstate', this.onPopState)
-  }
-  componentWillReceiveProps (nextProps) {
-    if (this.context.router && this.props.location.search !== nextProps.location.search) this.onPopState()
-  }
-  onPopState = () => {
-    this.props.dispatch(initSearch({ config: this.context.config }))
-  };
-  render () {
-    return <ComposedComponent {...this.props} />
-  }
+  return hoistNonReactStatics(OlaRoute, WrappedComponent)
 }
