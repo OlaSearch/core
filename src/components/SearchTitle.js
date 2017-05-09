003632a285184620fba81fd0f8909c92e0d4f7e7
Vinay <rmdort@gmail.com>
2017-05-09 10:41:05 +0800

    Updated search title

--- src/components/SearchTitle.js
+++ src/components/SearchTitle.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types'
 import injectTranslate from './../decorators/OlaTranslate'
 
 const SearchTitle = ({ totalResults, page, perPage, isPhone, translate }, context) => {
+  let title = translate('title')
+  let showTitle = title || totalResults > 0
+  if (!showTitle) return null
   let currentIdx = Math.min(((page - 1) * perPage) + 1, totalResults)
   let lastIdx = Math.min(totalResults, currentIdx + parseInt(perPage) - 1)
 
@@ -14,11 +17,13 @@ const SearchTitle = ({ totalResults, page, perPage, isPhone, translate }, contex
     total: totalResults
   }
   let titleDesc = totalResults ? translate('showing', values) : translate('showing_no_results', values)
-  let title = translate('title')
   return (
     <h3 className='ola-search-heading'>
       {title && <span className='ola-search-heading-title'>{title}</span>}
-      <small className='ola-search-heading-number'>{titleDesc}</small>
+      {totalResults
+        ? <small className='ola-search-heading-number'>{titleDesc}</small>
+        : null
+      }
     </h3>
   )
 }
