ac696de182b7cc06c3106c9b5f3d58c1c595e705
Vinay <rmdort@gmail.com>
2017-07-14 12:08:25 +0800

    Added dependent field to date

--- src/components/Fields/Date.js
+++ src/components/Fields/Date.js
@@ -2,8 +2,21 @@ import React from 'react'
 import DateParser from './../../utilities/dateParser'
 import FieldLabel from './FieldLabel'
 
-const DateField = ({ date, format, fieldLabel }) => {
+const DateField = ({ date, format, fieldLabel, dependentField, result, showIfEmpty }) => {
   if (!date) return null
+  if (dependentField) {
+    if (dependentField === date) {
+      if (showIfEmpty) {
+        return (
+          <div className='ola-field ola-field-date'>
+            <FieldLabel label={fieldLabel} />
+            -
+          </div>
+        )
+      }
+      return null
+    }
+  }
   let formattedDate = ''
   try {
     formattedDate = DateParser.format(date, format)
@@ -16,6 +29,11 @@ const DateField = ({ date, format, fieldLabel }) => {
       {formattedDate}
     </div>
   )
+}
+
+DateField.defaultProps = {
+  dependentField: null,
+  showIfEmpty: false
 }
 
 module.exports = DateField
--- src/constants/Version.js
+++ src/constants/Version.js
@@ -1,1 +1,1 @@
-module.exports = '1.6.5'
+module.exports = '1.6.6'
