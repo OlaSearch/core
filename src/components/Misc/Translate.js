346168c76114015bdd7e0202a1ec1bd3dfea4042
Vinay <rmdort@gmail.com>
2017-07-25 17:54:36 +0800

    Minor update

--- src/components/Misc/Translate.js
+++ src/components/Misc/Translate.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import injectTranslate from './../../decorators/OlaTranslate'
 
-const Translate = ({ id, translate, tagName = 'span' }) => {
+const Translate = ({ id, translate, tagName }) => {
   return React.createElement(tagName, null, translate(id))
 }
+
+Translate.defaultProps = {
+  tagName: 'span'
+};
 
 module.exports = injectTranslate(Translate)
