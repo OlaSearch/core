50d2512652363f42f0f470bb1b5da23b64990e09
Vinay M <rmdort@gmail.com>
2017-11-30 21:59:22 +0800

    Test

--- src/components/Answer/common/ItemDetail.js
+++ src/components/Answer/common/ItemDetail.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ItemDetail ({ data: { description, title } }) {
   return (
-    <div className='ola-answer-itemdetail'>
+    <div className='ola-answer-itemdetails'>
       <div className='ola-answer-title'>{title}</div>
       <div className='ola-answer-description'>{description}</div>
     </div>
